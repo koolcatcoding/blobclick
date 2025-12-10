f9db57f3489e2ab357ac09d3a274d4dd5eeaa1d7
koolcatcoding <koolcatkrome@gmail.com>
2025-12-10 16:30:58 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,50 @@
+
+
+// hi! this a comment. when you see the quadruple slash(////), that comment is just there to disable a line of script
+function SAVE(){
+ localStorage.setItem("blorbs", clicks);
+}
+
+
+
+
+function click(){
+    clicks = clicks+clp
+    document.getElementById(2222).innerHTML = clicks
+    document.getElementById(2223).innerHTML = clicks
+}
+// the formatting might be weird because I have my text editor set to that each line of code starts offset to the right
+ function RESET(){
+            clicks = 0
+            clp = 1
+            co=10              
+            document.getElementById('num').innerHTML = "resetted";
+            document.getElementById('nuum').innerHTML = "none";
+//seperator
+function upone(){
+    
+    
+                if(clicks>co-1){clicks=clicks-co
+                    // just an extra line.
+                    clp=clp+1;co=co*1.2
+                    document.getElementById(2223).innerHTML = clicks
+                    document.getElementById(2222).innerHTML = clicks
+                    document.getElementById(3).innerHTML=co
+            }}
+// event listener for the go button on the index page
+  function go(){
+   var name = document.getElementById("nam")
+   var clicks = document.getElementById("blorbnum") 
+  }
+
+function load(){
+    clicks = localStorage.getItem("blorbs");
+
+}
+
+
+
+
+
+
+    
